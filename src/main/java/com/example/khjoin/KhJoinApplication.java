8e51d422255183a31cd772fb283274decf0edce1
anhyeongkwan <bombom841@naver.com>
2023-12-03 02:36:23 +0900

    - querydsl build까지

---
+++ src/main/java/com/example/khjoin/KhJoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.khjoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhJoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/khjoin/KhJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.khjoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhJoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
